a3aaddfc8c400eef2d4a90e0fd0dca69b59fcd68
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-09-27 16:02:28 -0400

    more playing in react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import React, { StrictMode } from "react";
 import "./styles.css";
+import TaskBar from './TaskBar'
+
 
 import Square from "./App";
 
@@ -8,5 +10,6 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <Square />
+    <TaskBar />
   </StrictMode>
 );
